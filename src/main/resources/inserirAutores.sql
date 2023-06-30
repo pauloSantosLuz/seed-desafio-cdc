df4498904e58f403c27d463cf32faa42705ded3f
paulo <paulo.contact.by@gmail.com>
2023-06-29 22:00:52 -0300

    wip

--- src/main/resources/inserirAutores.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO Autor (nome, descricao, email, instante) VALUES
-  ('John Doe', 'Escritor renomado com diversas publicações', 'johndoe@example.com', CURRENT_DATE),
-  ('Jane Smith', 'Autora de best-sellers de ficção científica', 'janesmith@example.com', CURRENT_DATE),
-  ('Michael Johnson', 'Palestrante e especialista em autoajuda', 'michaeljohnson@example.com', CURRENT_DATE);
--- src/test/kotlin/com/paulo/cdc/SqlTestProvider.kt
+++ src/test/kotlin/com/paulo/cdc/SqlTestProvider.kt
@@ -2,7 +2,7 @@ package com.paulo.cdc
 
 class SqlTestProvider {
     companion object {
-        private const val PATH = "/resources"
+        private const val PATH = "classpath:"
 
         const val INSERIR_AUTORES = "$PATH/inserirAutores.sql"
 
